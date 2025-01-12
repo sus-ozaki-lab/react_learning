2b063780785caefbf41f4eed88733d06e9f3baf6
T122060 <maziqiteng@gmail.com>
2025-01-12 19:04:46 +0900

    add:Homeのページ

---
+++ div/react-app/src/page/Home.js
@@ -0,0 +1,14 @@
+// Home.js
+import React, { useState } from "react";
+
+const Home = () => {
+  const labID = "尾崎研究室"; // 仮のログイン情報
+
+  return (
+    <div>
+      <h1>研究室 鍵管理システム</h1>
+    </div>
+  );
+};
+
+export default Home;
