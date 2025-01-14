b57d4b03863b8dd5c3a02a8d2ad08850c5996461
T122104 <1215yucka@gmail.com>
2025-01-14 23:59:17 +0900

    fix: リンクを修正

--- div/react-app/src/App.js
+++ div/react-app/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./page/Homeome";
+import Home from "./page/Home";
 import KeyPlace from "./page/keyPlace";
 
 function App() {
